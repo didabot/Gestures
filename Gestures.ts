32ff1e125fdc0f148855f1901a3a06269ec90409
IIQAQII <40288683+IIQAQII@users.noreply.github.com>
2020-08-19 15:21:19 +0800

    Add files via upload

--- Gestures.ts
+++ Gestures.ts
@@ -1,7 +1,7 @@
 
 
-//% color=#FF8C00  icon="\uf110" block="Gestures" blockId="Gestures"
-namespace Gestures {
+//% weight=5 color=#9900CC icon="\uf53b"
+namespace Gesture {
     ////////////////////////paj7620//////////////////////
     let gesture_first_init = true
     const initRegisterArray: number[] = [
@@ -141,6 +141,7 @@ namespace Gestures {
     */
     //% blockId= gesture_create_event block="Gesture sensor IIC port is %gesture"
     //% gesture.fieldEditor="gridpicker" gesture.fieldOptions.columns=3
+    //% subcategory=Sensor group="IIC Port"
     export function onGesture(gesture: GestureType, handler: () => void) {
         control.onEvent(gestureEventId, gesture, handler);
         if(gesture_first_init){
