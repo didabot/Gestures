f7f9e09434b64ccf287fa61368a7cf38e4f0b6d4
IIQAQII <40288683+IIQAQII@users.noreply.github.com>
2020-08-19 17:08:53 +0800

    Add files via upload

--- Gestures.ts
+++ Gestures.ts
@@ -1,6 +1,6 @@
 
 
-//% color=#FF8C00  icon="\uf110" block="Gestures" blockId="Gestures"
+//% color=#FF8C00  icon="\uf110" block="Gestureis" blockId="Gestureis"
 namespace Gestures {
     ////////////////////////paj7620//////////////////////
     let gesture_first_init = true
